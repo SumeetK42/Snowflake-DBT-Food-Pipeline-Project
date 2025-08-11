9ca249f74253016e3cc9f5bfa0d5d2283bb0b859
SumeetK42 <sumeet.karmankar42@gmail.com>
2025-08-11 17:14:02 +0530

    email_tranformation macro added

--- food_app_project/macros/handle_email.sql
+++ food_app_project/macros/handle_email.sql
@@ -1,5 +1,8 @@
 {% macro handle_email(column_name) %}
 
-{{ column_name  }}
+case 
+   when regexp_like( trim({{ column_name  }}),'([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+)\.(com)') then lower(trim({{ column_name  }}))
+   else 'Not Available'
+end 
 
 {% endmacro %}
