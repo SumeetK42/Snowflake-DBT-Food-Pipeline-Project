bcaea0504eba467d7a8ad8b44de64b0b400b658c
SumeetK42 <sumeet.karmankar42@gmail.com>
2025-09-03 11:15:49 +0530

    added menu dim

--- food_app_project/models/reporting_layer/CUSTOMER_DIM.sql
+++ food_app_project/models/reporting_layer/CUSTOMER_DIM.sql
@@ -14,21 +14,3 @@ CUST.DBT_VALID_FROM as EFF_START_DATE,
 CUST.DBT_VALID_TO AS EFF_END_DATE,
 {{ handle_is_current("DBT_VALID_TO") }} as IS_CURRENT
 from {{ ref('customer_snapshot') }} CUST
-
-/*
-
- CUSTOMER_HK NUMBER PRIMARY KEY,               -- Surrogate key for the customer
-    CUSTOMER_ID STRING NOT NULL,                                 -- Natural key for the customer
-    NAME STRING(100) NOT NULL,                                   -- Customer name
-    MOBILE STRING(15) WITH TAG (common.pii_policy_tag = 'PII'),                                           -- Mobile number
-    EMAIL STRING(100) WITH TAG (common.pii_policy_tag = 'EMAIL'),                                           -- Email
-    LOGIN_BY_USING STRING(50),                                   -- Method of login
-    GENDER STRING(10) WITH TAG (common.pii_policy_tag = 'PII'),                                           -- Gender
-    DOB DATE WITH TAG (common.pii_policy_tag = 'PII'),                                                    -- Date of birth
-    ANNIVERSARY DATE,                                            -- Anniversary
-    PREFERENCES STRING,                                          -- Preferences
-    EFF_START_DATE TIMESTAMP_TZ,                                 -- Effective start date
-    EFF_END_DATE TIMESTAMP_TZ,                                   -- Effective end date (NULL if active)
-    IS_CURRENT BOOLEAN   
-
-*/
--- food_app_project/models/reporting_layer/menu_dim.sql
+++ food_app_project/models/reporting_layer/menu_dim.sql
@@ -1,35 +1,14 @@
-{{ 
-    config(materialized = 'table')
-}}
 
-with menu_snap as (
-  select * from {{ ref('menu_snapshot') }}
-)
 select 
-{{ add_surrograte_key(MENU_ID,RESTAURANT_ID,MENU_ITEM_NAME) }} as MENU_SK,
+{{ add_surrograte_key("MENU_ID","RESTAURANT_ID","MENU_ITEM_NAME") }} as MENU_SK,
 MENU_ID,
 RESTAURANT_ID,
 MENU_ITEM_NAME,
 DESCRIPTION,
 PRICE,
 CATEGORY,
-IS_AVAILABLE_NOW
-from menu_snap
-
-/*
-
-
-  Menu_Dim_HK NUMBER primary key comment 'Menu Dim HK (EDW)',                         -- Hash key generated for Menu Dim table
-    Menu_ID INT NOT NULL comment 'Primary Key (Source System)',                       -- Unique and non-null Menu_ID
-    Restaurant_ID_FK INT NOT NULL comment 'Restaurant FK (Source System)',                          -- Identifier for the restaurant
-    Item_Name STRING,                            -- Name of the menu item
-    Description STRING,                         -- Description of the menu item
-    Price DECIMAL(10, 2),                       -- Price as a numeric value with 2 decimal places
-    Category STRING,                            -- Food category (e.g., North Indian)
-    Availability BOOLEAN,                       -- Availability status (True/False)
-    Item_Type STRING,                           -- Dietary classification (e.g., Vegan)
-    EFF_START_DATE TIMESTAMP_NTZ,               -- Effective start date of the record
-    EFF_END_DATE TIMESTAMP_NTZ,                 -- Effective end date of the record
-    IS_CURRENT BOOLEAN                         -- Flag to indicate if the record is current (True/False)
-
-*/
+IS_AVAILABLE_NOW,
+DBT_VALID_FROM AS EFF_START_DATE,
+DBT_VALID_TO AS EFF_END_DATE,
+{{ handle_is_current("DBT_VALID_TO") }} AS IS_CURRENT
+from {{ ref('menu_snapshot') }}
